7770a4c1a0105ac08a5737ea02f982c17239b6e8
Jacob Turcotte <jacobturcotte20@gmail.com>
2025-11-13 07:47:04 -0500

    Day 7 fors and whiles. Work mostly as expected.

--- Learnxinyminutes Python/Chapter 3 Control Flow and Iterables/Control Flow and Iterables.py
+++ Learnxinyminutes Python/Chapter 3 Control Flow and Iterables/Control Flow and Iterables.py
@@ -33,3 +33,78 @@ match command:
         print("Invalid command")
     
 # Output: "The robot started to run"
+
+
+
+# for loops
+"""
+prints:
+    dog is a mammal
+    cat is a mammal
+    mouse is a mammal
+"""
+
+for animal in ["dog", "cat", "mouse"]:
+    # You can use format() to interpolate formatted strings
+    print("{} is a mammal".format(animal))
+
+"""
+range(number) returns an iterable of numbers from zero up to (but excluding) the given number
+    
+prints:
+    0
+    1
+    2
+    3
+"""
+
+for i in range(4):
+    print(i)
+
+"""
+range(lower,upper) returns an iterable of numbers from zero up to (but excluding) the upper number
+prints:
+    4
+    5
+    6
+    7
+"""
+
+for i in range(4, 8):
+    print(i)
+
+"""
+range(lower, upper, step) returns an interable numbers from the lower number to the upper number, incrementing by the step.
+If not defined, default step is 1
+prints:
+    4
+    6
+"""
+
+for i in range(4, 8, 2):
+    print(i)
+
+"""
+Loop over a list to retrieve both index and value of each list item
+    0 dog
+    1 cat
+    2 mouse
+"""
+animals = ["dog", "cat", "mouse"]
+for i, value in enumerate(animals):
+    print(i, value)
+
+"""
+while loops work as expected, loop until no longer true
+prints:
+    0
+    1
+    2
+    3
+"""
+x = 0
+while x < 4:
+    print(x)
+    x += 1  # Note that x++ does not work in Python because variables are pointers, not values.
+
+
